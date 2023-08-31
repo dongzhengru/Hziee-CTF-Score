ffaeba3ea49e4ae8d66efd7e584d839d10bd2d5c
dongzhengru <zhengru05@126.com>
2023-08-31 13:11:54 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Hziee-CTF-Score
+一个网络安全知识赛练习平台的工具。
+
+本脚本仅供个人学习，造成任何后果均与作者无关。
+
+------
+
 A tool for the practice platform of network security knowledge competition.
+
+This script is for personal study only, and any consequences have nothing to do with the author.
---
+++ main.py
@@ -0,0 +1,144 @@
+import json
+import msvcrt
+import requests
+
+def getToken(username, pwd):
+    url = "http://124.160.107.92:3334/api/login"
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = {
+        "account": username,
+        "password": pwd,
+        "isRemember": True
+    }
+    resp = requests.post(url, headers=headers, json=data)
+    return json.loads(resp.text)['data']['token']
+
+def getName(tk,xh):
+    url = "http://124.160.107.92:3334/api/info"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": tk
+    }
+    data = {
+        "account": xh
+    }
+    resp = requests.post(url, headers=headers, json=data)
+    return json.loads(resp.text)['data']['RealName']
+
+def commit(qid, answer, token):
+    url = "http://124.160.107.92:3334/api/study/questionEval"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": token
+    }
+    data = {
+        "id": qid,
+        "answer": answer
+    }
+    resp = requests.post(url, headers=headers, json=data)
+    if resp.text == '{"code":10000,"msg":"成功","data":{"result":true},"error":"","succeed":true}':
+        print("题目id：" + str(qid) + "  答案：" + answer + "  答题成功！积分+1")
+
+def getanswer(qid, token):
+    url = "http://124.160.107.92:3334/api/study/questionDetail"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": token
+    }
+    data = {
+        "id": qid
+    }
+    resp = requests.post(url, headers=headers, json=data)
+    detail_data = json.loads(resp.text)
+    return detail_data["data"]["answer"]
+
+def getanswer(token, stuid):
+    url = "http://124.160.107.92:3334/api/contest/paper/getUserDetail"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": token
+    }
+    data = {
+        "id": stuid
+    }
+    resp = requests.post(url, headers=headers, json=data)
+    return json.loads(resp.text)['data']['content']
+
+def addtmp(token, ans, id, remainat):
+    url = "http://124.160.107.92:3334/api/contest/paper/addTmp"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": token
+    }
+    data = {
+        "content": ans,
+        "id": id,
+        remainat: remainat
+    }
+    resp = requests.post(url, headers=headers, json=data)
+    if resp.text == '{"code":10000,"msg":"成功","data":{"result":true},"error":"","succeed":true}':
+        print("操作成功！")
+        print("注意：为了避免雷同，请务必修改答案，再进行手动提交！")
+        print("注意：为了避免雷同，请务必修改答案，再进行手动提交！")
+        print("注意：为了避免雷同，请务必修改答案，再进行手动提交！")
+        print("注意：为了避免雷同，请务必修改答案，再进行手动提交！")
+        print("注意：为了避免雷同，请务必修改答案，再进行手动提交！")
+
+def quickpractice(tk):
+    print("欢迎使用普通积分模式")
+    num = input("请输入目标积分（大于10）：")
+    for qid in range(125, 134):
+        commit(qid, getanswer(qid, tk), tk)
+    for qid in range(831, int(num) + 831):
+        commit(qid, getanswer(qid, tk), tk)
+
+def quickexam(tk):
+    print("欢迎使用紧急考试模式")
+    print("=======================================")
+    print("注意：请先关闭考试页面，否则以下操作无效！")
+    print("注意：请先关闭考试页面，否则以下操作无效！")
+    print("注意：请先关闭考试页面，否则以下操作无效！")
+    print("=======================================")
+    stuid = input("请输入目标ID（必须联系作者获取！）：")
+    examid = input("请输入考试ID（必须联系作者获取！）：")
+    remainat = input("请输入神秘代码（必须联系作者获取！）：")
+    print("=======================================")
+    ans = getanswer(tk, int(stuid))
+    addtmp(tk, ans, int(examid), int(remainat))
+
+
+print("  ______          _              _____ _______ ______ ")
+print(" |  ____|        | |            / ____|__   __|  ____|")
+print(" | |__ _   _  ___| | __ ______ | |       | |  | |__   ")
+print(" |  __| | | |/ __| |/ / ______ | |       | |  |  __|  ")
+print(" | |  | |_| | (__|   <         | |____   | |  | |     ")
+print(" |_|   \__,_|\___|_|\_\         \_____|  |_|  |_|     ")
+print("               v1.0                  ")
+print("       仅供学习，请勿转载 ——ByZR        ")
+print("                                     ")
+xh = input("请输入学号：")
+print("默认密码为Hziee学号后六位_")
+password = input("请输入密码：")
+tk = getToken(xh, password)
+if tk != '':
+    print("=======================================")
+    print(getName(tk, xh) + " 登录成功")
+    print("=======================================")
+    print("本脚本仅供个人学习，造成任何后果均与作者无关。")
+    print("本脚本仅供个人学习，造成任何后果均与作者无关。")
+    print("本脚本仅供个人学习，造成任何后果均与作者无关。")
+    print("=======================================")
+    print("1 普通积分模式")
+    print("2 紧急考试模式")
+    print("=======================================")
+    mode = input("请选择模式：")
+    print("=======================================")
+    if mode == '1':
+        quickpractice(tk)
+    elif mode == '2':
+        quickexam(tk)
+print("=======================================")
+print("按下任意键退出...")
+msvcrt.getch()
